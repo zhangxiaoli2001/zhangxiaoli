9df137eead04b659473ce1980d7d72f399d3b5d6
zhangxiaoli2001 <1936527701@qq.com>
2026-07-07 15:40:35 +0800

    first commit

---
+++ tijiao_python_cd.py
@@ -0,0 +1,9 @@
+#学习git commad
+print("hello world")
+cubes = [i ** 3 for i in range(10)]
+for number in range(100, 1000):
+    hundreds = number // 100
+    tens = number // 10 % 10
+    ones = number % 10
+    if cubes[hundreds] + cubes[tens] + cubes[ones] == number:
+        print(number)
